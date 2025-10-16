135908ecc3355bc861be92777bd5c2d62126522d
Vishulover680 <vishal255g@gmail.com>
2025-10-16 23:41:04 +0530

    Update OWNER_ID and SUPPORT_CHAT values

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @sweetheart07_bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID",8227673917))
+OWNER_ID = int(getenv("OWNER_ID",2241213492))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shayrirahulki")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Pallavisarkaar")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Galiyokabadshah")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
