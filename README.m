02b44ab6ddc2a5c3a5b7b91bf9a52a3dc2b17784
Vishulover680 <vishal255g@gmail.com>
2025-10-16 21:40:09 +0530

    Update Heroku template link in README

--- README.m
+++ README.m
@@ -1,2 +1,2 @@
 https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB
-https://dashboard.heroku.com/new?template=https://github.com/vishumusic/vishuuuu
+https://dashboard.heroku.com/new?template=https://github.com/vishu007vn/Vishu
