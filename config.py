8f0d52356471c17541aa82edae0c8c36dad4bc6d
Vishulover680 <vishal255g@gmail.com>
2025-10-16 09:27:32 +0530

    Update configuration values for owner and URLs

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @purvi_music_bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7668520999))
+OWNER_ID = int(getenv("OWNER_ID",8457570997))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,14 +32,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vishu007vn/Pallavi_Music",
+    "https://github.com/vishu007vn/Vishu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Pallavisarkaar")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shayrirahulki")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Pallavisarkaar")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/dahjy3.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/0rm53h.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/d6b8j1.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/0rm53h.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/dahjy3.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/dahjy3.jpg"
